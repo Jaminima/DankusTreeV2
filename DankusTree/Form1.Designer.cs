8518c976dac99f6d3f2dd2d9cf1e4fdfe4503550
Oscar Davies <oscar.william.davies@outlook.com>
2018-11-08 21:19:48 +0000

    Basic features

---
+++ DankusTree/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace DankusTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Label_Time = new System.Windows.Forms.Label();
+            this.Timer_Time = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.Timer_Lights = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(416, 496);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Label_Time
+            // 
+            this.Label_Time.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Label_Time.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Time.Location = new System.Drawing.Point(0, 499);
+            this.Label_Time.Name = "Label_Time";
+            this.Label_Time.Size = new System.Drawing.Size(416, 53);
+            this.Label_Time.TabIndex = 1;
+            this.Label_Time.Text = "Label_time";
+            this.Label_Time.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Timer_Time
+            // 
+            this.Timer_Time.Enabled = true;
+            this.Timer_Time.Tick += new System.EventHandler(this.Timer_Time_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(21, 386);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(15, 15);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Location = new System.Drawing.Point(114, 322);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(15, 15);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Red;
+            this.panel3.Location = new System.Drawing.Point(256, 227);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(15, 15);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Location = new System.Drawing.Point(127, 206);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(15, 15);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Red;
+            this.panel5.Location = new System.Drawing.Point(223, 123);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(15, 15);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Red;
+            this.panel6.Location = new System.Drawing.Point(159, 84);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(15, 15);
+            this.panel6.TabIndex = 4;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Red;
+            this.panel7.Location = new System.Drawing.Point(289, 340);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(15, 15);
+            this.panel7.TabIndex = 4;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Red;
+            this.panel8.Location = new System.Drawing.Point(302, 412);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(15, 15);
+            this.panel8.TabIndex = 4;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Red;
+            this.panel9.Location = new System.Drawing.Point(185, 372);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(15, 15);
+            this.panel9.TabIndex = 4;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.Red;
+            this.panel10.Location = new System.Drawing.Point(276, 174);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(15, 15);
+            this.panel10.TabIndex = 4;
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.Color.Red;
+            this.panel11.Location = new System.Drawing.Point(204, 278);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(15, 15);
+            this.panel11.TabIndex = 4;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.Color.Red;
+            this.panel12.Location = new System.Drawing.Point(114, 427);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(15, 15);
+            this.panel12.TabIndex = 4;
+            // 
+            // panel13
+            // 
+            this.panel13.BackColor = System.Drawing.Color.Red;
+            this.panel13.Location = new System.Drawing.Point(169, 160);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(15, 15);
+            this.panel13.TabIndex = 4;
+            // 
+            // Timer_Lights
+            // 
+            this.Timer_Lights.Enabled = true;
+            this.Timer_Lights.Interval = 250;
+            this.Timer_Lights.Tick += new System.EventHandler(this.Timer_Lights_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(416, 561);
+            this.Controls.Add(this.panel13);
+            this.Controls.Add(this.panel12);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Label_Time);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Dankus Tree";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Label_Time;
+        private System.Windows.Forms.Timer Timer_Time;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Timer Timer_Lights;
+    }
+}
+
---
+++ DankusTree/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DankusTree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Timer_Time_Tick(object sender, EventArgs e)
+        {
+            TimeSpan TSpan = (TimeSpan)(new DateTime(DateTime.Now.Year,12,25,0,0,0) - DateTime.Now);
+            string SSpan = "";
+            if (Math.Floor(TSpan.TotalDays) != 0) { SSpan += Math.Floor(TSpan.TotalDays) + " Days "; }
+            if (TSpan.Hours != 0) { SSpan += TSpan.Hours + " Hours "; }
+            if (TSpan.Minutes != 0) { SSpan += TSpan.Minutes + " Mins "; }
+            if (SSpan != "") { SSpan += "And "; }
+            SSpan += TSpan.Seconds + " Secs ";
+            Label_Time.Text = SSpan;
+        }
+
+        Random Rnd = new Random();
+        Color[] Colors = new Color[] { Color.Red,Color.Green,Color.Cyan,Color.Gold };
+        private void Timer_Lights_Tick(object sender, EventArgs e)
+        {
+            foreach (Control Control in this.Controls)
+            {
+                if (Control.GetType()==new Panel().GetType())
+                {
+                    Control.BackColor = Colors[Rnd.Next(0, Colors.Length)];
+                }
+            }
+        }
+    }
+}
